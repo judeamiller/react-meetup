70897e70ceccd6ee304ca1d703c7f0e288ba13a5
Jude Baca-Miller <judeamiller@gmail.com>
2018-10-10 21:33:54 -0600

    add code for firebase posting and retreiving messages from firebase.

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import ChatWindow from './ChatWindow';
 import ChatInput from './ChatInput';
+import styled from 'styled-components';
+import firebase from './firebase';
+
+
+const MainContainer = styled.div`
+  width: 100%;
+  background-color: black;
+  color: white;
+  padding: 50px;
+`;
 
 class App extends Component {
   constructor(props) {
@@ -16,16 +26,40 @@ class App extends Component {
       }
   }
 
+  componentDidMount = () =>  {
+    const messagesRef = firebase.database().ref('messages');
+
+    messagesRef.on('value', (snapshot) => {
+      let messages = [];
+      snapshot.forEach(element=> {
+        messages.push(`${element.val().username}: ${element.val().message}`);
+      })
+      this.setState({
+        messages,
+      })
+    });
+  }
+  
   addNewMessage = message => {
-    this.setState({messages: [...this.state.messages , message]});
+    const messagesRef = firebase.database().ref('messages');
+    messagesRef.push({
+      username: 'Jude',
+      message,
+    }, function(error){
+      if (error) {
+        console.log(error)
+      } else {
+        console.log('success');
+      }
+    })
   }
 
   render() {
     return (
-      <div className="App">
+      <MainContainer className="App">
        <ChatWindow messages={this.state.messages} />
        <ChatInput addNewMessage={this.addNewMessage}/>
-      </div>
+      </MainContainer>
     );
   }
 }
